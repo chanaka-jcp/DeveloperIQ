bc8a89080778f5e083b42c561c88c6b51fe2cee8
chanaka-jcp <gunawardhanajcp@gmail.com>
2023-12-11 13:21:47 +0530

    testing test-run script

--- test-run.ps1
+++ test-run.ps1
@@ -1,7 +1,7 @@
 Write-Host "Running tests..."
 
 try {
-    Invoke-WebRequest -Uri http://20.121.169.237:802/ -UseBasicParsing -TimeoutSec 30
+    Invoke-WebRequest -Uri http://20.121.169.239:802/ -UseBasicParsing -TimeoutSec 30
     Write-Host "Tests passed."
 }
 catch {
