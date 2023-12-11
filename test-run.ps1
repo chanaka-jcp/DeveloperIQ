111b197025df60459f2cc94963e02d0cc49bb7bb
chanaka-jcp <gunawardhanajcp@gmail.com>
2023-12-11 12:52:27 +0530

    testing the test run script

--- test-run.ps1
+++ test-run.ps1
@@ -1,6 +1,10 @@
-# test-run.ps1
+Write-Host "Running tests..."
 
-Write-Output "Running tests..."
-
-# Add testing commands below
-Invoke-WebRequest -Uri http://20.121.169.237:802/
+try {
+    Invoke-WebRequest -Uri http://20.121.169.237:802/ -UseBasicParsing -TimeoutSec 30
+    Write-Host "Tests passed."
+}
+catch {
+    Write-Host "Tests failed: $_"
+    exit 1
+}
